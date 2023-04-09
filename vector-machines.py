24a92db6d72278c8598f38165ee78e08fe6e4bf0
Ricardo Ferrari <ricardoaugusto.ferrari@gmail.com>
2023-04-09 12:52:53 -0300

    feat: adicionada matriz de confusão

--- vector-machines.py
+++ vector-machines.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report, ConfusionMatrixDisplay
 from sklearn import svm
 
 import seaborn as sns
@@ -59,13 +59,13 @@ for ix, (x, y) in enumerate(((w_train, original_y_train), (w_test, original_y_te
 plt.figure(figsize=(16, 4))
 
 plt.subplot(121)
-plt.title('Frequencia das classes no conjunto de treinamento (%i amostras)' % len(x_train))
+plt.title('Frequencia das classes no conjunto de treinamento (%i amostras)' % len(original_x_train))
 labels, counts = np.unique(original_y_train, return_counts=True)
 sns.barplot(x=labels, y=counts)
 
 plt.subplot(122)
-plt.title('Frequencia das classes no conjunto de teste (%i amostras)' % len(x_test))
-labels, counts = np.unique(y_test, return_counts=True)
+plt.title('Frequencia das classes no conjunto de teste (%i amostras)' % len(original_x_test))
+labels, counts = np.unique(original_y_test, return_counts=True)
 sns.barplot(x=labels, y=counts);
 
 
@@ -93,31 +93,40 @@ def train_test_plot_rbf(data, penalty_array=[1e-3, 1e-2, 1e-1, 1.0, 10.0], gamma
         
         print("Acurácia treino: ", train_acc, " - acurácia teste: ", test_acc, " - gamma: ", gamma, " - pennalty: ", penalty)
         
-        _, axes = plt.subplots(nrows=1, ncols=5, figsize=(10, 3))
-        for ax, image, prediction in zip(axes, original_x_test, predicted_test[0:5]):
-            print(prediction)
-            ax.set_axis_off()
-            image = image.reshape(8, 8)
-            ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-            ax.set_title(f"Prediction: {prediction}")
 
-def show_predict_digits(data, penalty=1e-3, gamma=1e-1, kernel='rbf', num_test=5):
-    x_train, x_test, y_train, y_test = data
-    # plt.figure(figsize=(24, 18))
+
+def get_model_and_prediction(data, penalty=1e-3, gamma=1e-1, kernel='rbf'):
+    x_train, x_test, y_train, _ = data
     
     clf = svm.SVC(kernel=kernel, C=penalty, gamma=gamma)
     clf.fit(x_train, y_train)
 
     predicted_test = clf.predict(x_test)
+        
+    return (clf, predicted_test)
+
+# %%
+def show_predict_digits(predicted_test, num_test=5):
     
     _, axes = plt.subplots(nrows=1, ncols=num_test, figsize=(10, 3))
     for ax, image, prediction in zip(axes, original_x_test, predicted_test[0:num_test]):
-        print(prediction)
         ax.set_axis_off()
         image = image.reshape(8, 8)
         ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
         ax.set_title(f"Prediction: {prediction}")
 
+def calc_model_metrics(model, y_test, predicted):
+    print(
+        f"Classification report for classifier {model}:\n"
+        f"{classification_report(y_test, predicted)}\n"
+    )
+
+def plot_confusion_matrix(y_test, predicted):
+    disp = ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+    disp.figure_.suptitle("Matriz de confusão")
+    print(f"Matriz de confusão:\n{disp.confusion_matrix}")
+
+    plt.show()
 
 # %%
 
@@ -125,5 +134,14 @@ trainTestData = []
 trainTestData.append((original_x_train, original_x_test, original_y_train, original_y_test))
 
 train_test_plot_rbf(trainTestData[0],num_test = 10)
-show_predict_digits(trainTestData[0], num_test=5, penalty=1.0, gamma=0.001)
 
+best_model, best_prediction = get_model_and_prediction(trainTestData[0], penalty=1.0, gamma=0.001)
+
+
+# %%
+show_predict_digits(best_prediction, num_test=5)
+calc_model_metrics(best_model, original_y_test, best_prediction)
+plot_confusion_matrix(original_y_test, best_prediction)
+
+
+# %%
